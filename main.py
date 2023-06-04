5531c4783dd295f25f1eee3f3ca4af4d3612d065
choyeun <choyeun0372@gmail.com>
2023-06-04 20:05:10 +0900

    add post CREATE UPDATE DELETE

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -11,6 +11,8 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
 
 
 class User(Base):
@@ -26,6 +28,15 @@ class UserCreate(BaseModel):
     password: str
 
 
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(Text)
+    author_id = Column(Integer)
+
+
 Base.metadata.create_all(bind=engine)
 
 
@@ -33,26 +44,22 @@ def clear_database():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.post("/signup")
 def signup(user: UserCreate):
-    """
-    회원 가입을 처리하는 핸들러 함수입니다.
-
-    Parameters:
-        - user (UserCreate): 회원 가입 요청으로 받은 UserCreate 모델의 인스턴스입니다.
-
-    Returns:
-        - dict: 회원 가입 성공 여부와 메시지를 담은 응답 딕셔너리를 반환합니다.
-    """
     session = SessionLocal()
 
-    # 이미 등록된 사용자인지 확인
     existing_user = session.query(User).filter_by(username=user.username).first()
     if existing_user:
         return {"success": False, "message": "이미 등록된 사용자입니다."}
 
-    # 새로운 사용자 등록
     new_user = User(username=user.username, password=user.password)
     session.add(new_user)
     session.commit()
@@ -62,30 +69,76 @@ def signup(user: UserCreate):
 
 @app.post("/signin")
 def signin(user: UserCreate):
-    """
-    로그인을 처리하는 핸들러 함수입니다.
-
-    Parameters:
-        - user (UserCreate): 로그인 요청으로 받은 UserCreate 모델의 인스턴스입니다.
-
-    Returns:
-        - dict: 로그인 성공 여부와 메시지를 담은 응답 딕셔너리를 반환합니다.
-    """
     session = SessionLocal()
 
-    # 등록된 사용자인지 확인
     existing_user = session.query(User).filter_by(username=user.username).first()
     if not existing_user:
         return {"success": False, "message": "등록되지 않은 사용자입니다."}
 
-    # 비밀번호 일치 여부 확인
     if user.password != existing_user.password:
         return {"success": False, "message": "잘못된 비밀번호입니다."}
 
     return {"success": True, "message": "로그인 성공"}
+
+def get_current_user(username: str, password: str, db: SessionLocal = Depends(get_db)):
+    user = db.query(User).filter_by(username=username).first()
+    if not user or user.password != password:
+        raise HTTPException(status_code=401, detail="Invalid username or password")
+    return user
 
 
 @app.get("/clear")
 def clear():
     clear_database()
     return {"message": "Database cleared."}
+
+
+class PostCreate(BaseModel):
+    title: str
+    content: str
+
+
+@app.post("/posts")
+def create_post(post: PostCreate, user: User = Depends(get_current_user)):
+    session = SessionLocal()
+
+    new_post = Post(title=post.title, content=post.content, author_id=user.id)
+    session.add(new_post)
+    session.commit()
+
+    return {"message": "게시글이 생성되었습니다."}
+
+
+@app.put("/posts/{post_id}")
+def update_post(post_id: int, post: PostCreate, user: User = Depends(get_current_user)):
+    session = SessionLocal()
+
+    existing_post = session.query(Post).filter_by(id=post_id).first()
+    if not existing_post:
+        raise HTTPException(status_code=404, detail="게시글을 찾을 수 없습니다.")
+
+    if existing_post.author_id != user.id:
+        raise HTTPException(status_code=403, detail="게시글을 수정할 권한이 없습니다.")
+
+    existing_post.title = post.title
+    existing_post.content = post.content
+    session.commit()
+
+    return {"message": "게시글이 수정되었습니다."}
+
+
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: int, user: User = Depends(get_current_user)):
+    session = SessionLocal()
+
+    existing_post = session.query(Post).filter_by(id=post_id).first()
+    if not existing_post:
+        raise HTTPException(status_code=404, detail="게시글을 찾을 수 없습니다.")
+
+    if existing_post.author_id != user.id:
+        raise HTTPException(status_code=403, detail="게시글을 삭제할 권한이 없습니다.")
+
+    session.delete(existing_post)
+    session.commit()
+
+    return {"message": "게시글이 삭제되었습니다."}
